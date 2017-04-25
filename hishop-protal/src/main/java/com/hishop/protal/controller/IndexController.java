dac7cfd550b15f5905bb373808ac02359c75d285
你的名字 <912513404@qq.com>
2017-04-25 17:59:11 +0800

    主界面设计

---
+++ hishop-protal/src/main/java/com/hishop/protal/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.hishop.protal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String showIndex(){
+		return "index";
+	}
+}

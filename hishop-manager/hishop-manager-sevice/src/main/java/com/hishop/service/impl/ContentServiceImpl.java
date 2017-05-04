ae6ecaa8baacc12bb626301d2384d76f6e686c54
你的名字 <912513404@qq.com>
2017-05-04 18:06:03 +0800

    缓存机制的实现

--- hishop-manager/hishop-manager-sevice/src/main/java/com/hishop/service/impl/ContentServiceImpl.java
+++ hishop-manager/hishop-manager-sevice/src/main/java/com/hishop/service/impl/ContentServiceImpl.java
@@ -1,11 +1,14 @@
 package com.hishop.service.impl;
 
+import java.security.KeyStore.PrivateKeyEntry;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.hishop.common.util.HishopResult;
+import com.hishop.common.util.HttpClientUtil;
 import com.hishop.mapper.TbContentMapper;
 import com.hishop.pojo.TbContent;
 import com.hishop.service.ContentService;
@@ -14,13 +17,24 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper tbContentMapper;
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
 	@Override
 	public HishopResult insertContent(TbContent tbContent) {
 		// TODO Auto-generated method stub
 		tbContent.setCreated(new Date());
 		tbContent.setUpdated(new Date());
 		tbContentMapper.insert(tbContent);
-		return HishopResult.ok();
+		try {
+			HttpClientUtil.doGet(REST_BASE_URL+REST_CONTENT_SYNC_URL+tbContent.getCategoryId());
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+			return HishopResult.ok();
 	}
 
 }
